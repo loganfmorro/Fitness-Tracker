e0040d309a2a1014b9c54736185985204bc91b21
Logan Morro <loganfmorro@gmail.com>
2021-02-21 12:20:45 -0500

    adding seed files for our database

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const Workout = require('../models/workout.js');
 
 module.exports = function (app) {
 
-    //get workouts from the DB
+    //get workouts from the db
     app.get("/api/workouts", (req, res) => {
         Workout.find({}).then(dbWorkout => {
             console.log("WORKOUTS");
@@ -20,3 +20,39 @@ module.exports = function (app) {
             res.json(err);
         });
     });
+
+    // add an exercise to the db
+    app.put("/api/workouts/:id", (req, res) => {
+        Workout.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                $inc: { totalDuration: req.body.duration },
+                $push: { exercises: req.body }
+            },
+            { new: true }).then(dbWorkout => {
+                res.json(dbWorkout);
+            }).catch(err => {
+                res.json(err);
+            });
+    });
+
+     //create workout and post to db
+    app.post("/api/workouts", ({ body }, res) => {
+        Workout.create(body).then((dbWorkout => {
+            res.json(dbWorkout);
+        })).catch(err => {
+            res.json(err);
+        });
+    });
+
+     // get workouts in range from the db
+     app.get("/api/workouts/range", (req, res) => {
+        Workout.find({}).then(dbWorkout => {
+            console.log("ALL WORKOUTS");
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+}
