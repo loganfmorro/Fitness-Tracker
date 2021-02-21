b2a2ee789844adf015f8f41a7565fa7afba392c0
Logan Morro <loganfmorro@gmail.com>
2021-02-21 12:38:03 -0500

    adding server.js connection to begin testing

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const router = require("express").Router();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// updating to mongoDB constructor
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+// routes
+require('./routes/api-routes.js')(app);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
+
+module.exports = router;
